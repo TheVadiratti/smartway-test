c34333e2165c548cbcd8417298b9d5003c2784bf
Belousov Vadim <thevadiratti@yandex.ru>
2024-09-11 13:11:48 +0500

    Дополняет страницу NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,12 @@
+import PageContainer from '../../components/highOrder/PageContainer';
+import Link from '../../components/ui/Link';
+import routes from '../../router/routes';
+
 export default function NotFoundPage() {
   return (
-    <main>
+    <PageContainer>
       <h1>Страница не найдена.</h1>
-    </main>
+      <Link to={routes.main.index}>Вернутся на главную</Link>
+    </PageContainer>
   );
 }
